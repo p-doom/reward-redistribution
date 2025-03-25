b7d942f286347d352ea8b22994aa987b655be8aa
Mihir Mahajan <mihir.mahajan@tum.de>
2025-03-25 13:57:50 +0100

    wrote simple tests for advantage calculation as well

--- stoix/tests/multistep_test.py
+++ stoix/tests/multistep_test.py
@@ -12,9 +12,58 @@ from stoix.utils.multistep import (
 class TruncatedGeneralizedAdvantageEstimationTest(parameterized.TestCase):
     def setUp(self) -> None:
         super().setUp()
+    
+    @chex.all_variants()
+    def test_advantage_no_bootstrapping(self) -> None:
+        """Test GAE advantage outputs without value bootstrapping in a sparse reward setting.
+
+        We are using lambda=1 and discount (gamma)=1.
+        """
+        
+        r_t = jnp.array([[0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 1.0]])
+        v_t = jnp.ones((2, 6))
+        v_t = v_t.at[:, -1].set(0.0)
+
+        discount_t = jnp.ones_like(r_t)
+        truncation_t = jnp.array([[0.0, 0.0, 0.0, 0.0, 1.0], [0.0, 0.0, 0.0, 0.0, 0.0]])
+        
+        advantage_fn_variant = self.variant(batch_truncated_generalized_advantage_estimation)
+        advantages, targets = advantage_fn_variant(
+            r_t=r_t, discount_t=discount_t, lambda_=1.0, values=v_t, truncation_flags=truncation_t
+        )
+
+        expected_advantages = -jnp.array([[1.0, 1.0, 1.0, 1.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0]]) 
+        expected_targets = jnp.array([[0.0, 0.0, 0.0, 0.0, 0.0], [1.0, 1.0, 1.0, 1.0, 1.0]]) 
+
+        np.testing.assert_allclose(advantages, expected_advantages, atol=1e-3)
+        np.testing.assert_allclose(targets, expected_targets, atol=1e-3)
 
     @chex.all_variants()
-    def test_no_bootstrapping(self) -> None:
+    def test_advantage_with_bootstrapping(self) -> None:
+        """Test GAE advantage outputs with value bootstrapping in a sparse reward setting.
+
+        We are using lambda=1 and discount (gamma)=1.
+        """
+        
+        r_t = jnp.array([[0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 1.0]])
+        v_t = jnp.ones((2, 6))
+
+        discount_t = 1.-r_t
+        truncation_t = jnp.array([[0.0, 0.0, 0.0, 0.0, 1.0], [0.0, 0.0, 0.0, 0.0, 0.0]])
+        
+        advantage_fn_variant = self.variant(batch_truncated_generalized_advantage_estimation)
+        advantages, targets = advantage_fn_variant(
+            r_t=r_t, discount_t=discount_t, lambda_=1.0, values=v_t, truncation_flags=truncation_t
+        )
+
+        expected_advantages = jnp.zeros_like(r_t)
+        expected_targets = jnp.ones_like(r_t)
+
+        np.testing.assert_allclose(advantages, expected_advantages, atol=1e-3)
+        np.testing.assert_allclose(targets, expected_targets, atol=1e-3)
+
+    @chex.all_variants()
+    def _test_no_bootstrapping(self) -> None:
         """Test GAE without value bootstrapping in a sparse reward setting.
 
         We are using lambda=1 and discount (gamma)=1.
@@ -38,7 +87,7 @@ class TruncatedGeneralizedAdvantageEstimationTest(parameterized.TestCase):
         np.testing.assert_allclose(targets, expected_targets, atol=1e-3)
 
     @chex.all_variants()
-    def test_with_bootstrapping(self) -> None:
+    def _test_with_bootstrapping(self) -> None:
         """Test GAE with value bootstrapping in a sparse reward setting.
 
         We are using lambda=1 and discount (gamma)=1.
