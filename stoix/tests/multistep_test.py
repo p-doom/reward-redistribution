16486b31fcafb17db2bfa5ce1b6f74a897a989ab
mihir <78321484+maharajamihir@users.noreply.github.com>
2025-03-27 13:25:30 +0100

    Update stoix/tests/multistep_test.py
    Co-authored-by: Franz Srambical <79149449+emergenz@users.noreply.github.com>

--- stoix/tests/multistep_test.py
+++ stoix/tests/multistep_test.py
@@ -48,7 +48,7 @@ class TruncatedGeneralizedAdvantageEstimationTest(parameterized.TestCase):
     def test_advantage_with_bootstrapping(self) -> None:
         """Test GAE advantage outputs with value bootstrapping in a sparse reward setting.
 
-        We are using lambda=1 and discount (gamma)=1.
+        We are using lambda=1 and gamma=1.
         """
         
         r_t = jnp.array([[0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 1.0]])
